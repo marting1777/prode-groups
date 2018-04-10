af04b6b093a6496ca17b3f6a79d2e6b8bbb5fff9
martingalli <martin.galli@wunderman.com>
2018-04-10 10:20:07 -0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ let socket = io()
 
 socket.on('connect', function () {
     console.log('Connected to server')
-
-    socket.emit('createEmail', {
-        to: 'jen@example',
-        text: 'Hey. This is Martin'
-    })
 })
 
 socket.on('disconnect', function () {
     console.log('Disconnected from server')
 })
 
-socket.on('newEmail', function (email) {
-    console.log('New email', email)
+socket.on('newMessage', function (message) {
+    console.log('New message', message)
 })
